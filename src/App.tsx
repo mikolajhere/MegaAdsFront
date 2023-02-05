2544dafee4389de3aca1ad1ee0c1846e13343d50
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-02-05 20:24:25 +0100

    Add missing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TestInterface } from "types";
+
+//TODO 40:35
 
 function App() {
+  const foobar: TestInterface = {
+    x: 123,
+  };
   return (
     <div className="App">
       <header className="App-header">
